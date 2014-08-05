20d3a88f0d3f72ef660728bec37b1e9da39621d1
Javier Beneito Barquero <jbbarquero@gmail.com>
2014-08-05 10:12:41 +0200

    SPRING-BATCH-SAMPLE-0001: simple job configuration for a Person class that will be uppercased

---
+++ src/main/java/com/malsolo/springframework/batch/sample/PersonItemProcessor.java
@@ -0,0 +1,19 @@
+package com.malsolo.springframework.batch.sample;
+
+import org.springframework.batch.item.ItemProcessor;
+
+public class PersonItemProcessor implements ItemProcessor<Person, Person> {
+
+    @Override
+    public Person process(final Person person) throws Exception {
+        final String firstName = person.getFirstName().toUpperCase();
+        final String lastName = person.getLastName().toUpperCase();
+
+        final Person transformedPerson = new Person(firstName, lastName);
+
+        System.out.println("Converting (" + person + ") into (" + transformedPerson + ")");
+
+        return transformedPerson;
+    }
+
+}
